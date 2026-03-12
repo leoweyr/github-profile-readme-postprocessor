3bd5d2f933b15843daee8935b9a4c21c7d12b126
leoweyr <contact@leoweyr.com>
2026-03-12 19:44:20 +0800

    ci: fix terraform compatibility with AliCloud fc custom runtime

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     alicloud = {
       source  = "aliyun/alicloud"
@@ -9,40 +11,47 @@ terraform {
   backend "oss" {
     bucket = "odcime-terraform-state"
     prefix = "compute/github-profile-readme-postprocessor"
-    key    = "terraform.tfstate"
+    key = "terraform.tfstate"
     region = "cn-hangzhou"
+    encrypt = true
   }
 }
 
-provider "alicloud" {}
+provider "alicloud" {
+  region = local.region
+}
 
 variable "github_api_token" {
-  type      = string
+  type = string
   sensitive = true
 }
 
-data "alicloud_account" "current" {}
+locals {
+  region = "cn-hangzhou"
+  binary_name = "bootstrap"
+}
 
-data "archive_file" "bootstrap_zip" {
+data "archive_file" "function_zip" {
   type        = "zip"
-  source_file = "${path.module}/bootstrap"
-  output_path = "${path.module}/bootstrap.zip"
+  source_file = "${path.module}/${local.binary_name}"
+  output_path = "${path.module}/function_code.zip"
 }
 
 resource "alicloud_fc_service" "default" {
-  name    = "github-profile-readme-postprocessor"
-  publish = true
+  name        = "github-profile-readme-postprocessor"
+  publish     = true
 }
 
 resource "alicloud_fc_function" "default" {
   service     = alicloud_fc_service.default.name
   name        = "restful"
   runtime     = "custom.debian10"
-  handler     = "index.handler"  # Required but ignored by custom runtime.
+  handler     = "index.handler"
   memory_size = 128
   timeout     = 15
 
-  filename = data.archive_file.bootstrap_zip.output_path
+  filename      = data.archive_file.function_zip.output_path
+  code_checksum = data.archive_file.function_zip.output_base64sha256
 
   environment_variables = {
     FC_CUSTOM_LISTEN_PORT = "8080"
@@ -52,19 +61,17 @@ resource "alicloud_fc_function" "default" {
 }
 
 resource "alicloud_fc_trigger" "default" {
-  service       = alicloud_fc_service.default.name
-  function      = alicloud_fc_function.default.name
-  name          = "apisix-entrypoint"
-  type          = "http"
-  config_mocker = false
-  config = <<EOF
-  {
-      "authType": "anonymous",
-      "methods": ["GET"]
-  }
-  EOF
+  service  = alicloud_fc_service.default.name
+  function = alicloud_fc_function.default.name
+  name     = "apisix-entrypoint"
+  type     = "http"
+
+  config = jsonencode({
+    authType = "anonymous"
+    methods  = ["GET", "POST"]
+  })
 }
 
 output "fc_invoke_domain" {
-  value = "${data.alicloud_account.current.id}.cn-hangzhou.fc.aliyuncs.com"
+  value = "https://${alicloud_fc_service.default.name}.${local.region}.fc.aliyuncs.com/2016-08-15/proxy/${alicloud_fc_function.default.name}/${alicloud_fc_trigger.default.name}/"
 }
